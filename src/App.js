2b100165c09efcee15f9c5b61e0fb5a4e457a523
kiran <kletherwala@gmail.com>
2021-12-01 09:22:42 -0600

    everything working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import User from './components/User'
+import FollowerList from './components/FollowerList';
+
+
 
 class App extends React.Component {
+  state = {
+    currentUser: "kletherwala",
+    user: {
+      avatar_url: "https://avatars.githubusercontent.com/u/80646640?v=4",
+      html_url: "https://github.com/kletherwala",
+      name: "Kiran Letherwala",
+      login: "kletherwala",
+      public_repos: 31,
+      followers: 1,
+    },
+    followers: [
+      {
+        login: "SaltStalactite",
+        avatar_url: "https://avatars.githubusercontent.com/u/86366749?v=4",
+        html_url: "https://github.com/SaltStalactite"
+      }
+    ]
+  }
+
   render() {
     return(<div>
-      Github Card
+      <h1>Github Info</h1>
+      <form>
+        <input placeholder='Github Handle'/>
+        <button>Search</button>
+      </form>
+
+      <User user={this.state.user}/>
+      <FollowerList followers={this.state.followers}/>
+      
     </div>);
   }
 }
